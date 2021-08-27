43d16fae8d3e91829ae5d7eb2b701a7f49ead6f3
lorchiss <fabian.ortegav@gmail.com>
2021-08-27 14:54:01 -0400

    terminado

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+import operator
+
 nombreArchivo = "la voz de los 80.txt" 
 archivo = open(nombreArchivo, 'r')
 
-#print(archivo.read())
-
 lineas = archivo.readlines()
-#print(lineas) 
-print(lineas.count("Ya se siente la atmósfera"))
+
+cancion = {}
+
+for iter in lineas:
+    iter = iter.lower()
+    palabras = iter.split()
+    for palabra in palabras:
+        if palabra in cancion:
+            cancion[palabra] += 1
+        else:
+            cancion[palabra] = 1
+     
+
+cancionOrdenado = sorted(cancion.items(),key=operator.itemgetter(1), reverse=True)
+for palabra in cancionOrdenado:
+    print(palabra)
 
 archivo.close()
